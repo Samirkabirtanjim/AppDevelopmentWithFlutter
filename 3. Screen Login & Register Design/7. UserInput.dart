9e835f6a6e2a6db8a17cf90aa41c22f42b12f40a
Samir Kabir Tanjim <122144139+Samirkabirtanjim@users.noreply.github.com>
2026-06-12 21:11:39 +0600

    Add user input functionality in UserInput.dart

---
+++ 3. Screen Login & Register Design/7. UserInput.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+class Gettinginput extends StatefulWidget {
+  const Gettinginput({super.key});
+
+  @override
+  State<Gettinginput> createState() => _GettinginputState();
+}
+
+class _GettinginputState extends State<Gettinginput> {
+  TextEditingController myController = TextEditingController();
+
+  String user = "";
+  void getUser() {
+    setState(() {
+      user = myController.text;
+    });
+  }
+
+  String showText = "";
+  void show() {
+    setState(() {
+      showText = "Hello, " + user;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("My NotePad")),
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 50,),
+            Text(
+              "Hello, $user",
+              style: const TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: TextField(
+                controller: myController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+              ),
+            ),
+            ElevatedButton(onPressed: getUser, child: Text("Enter")),
+          ],
+        ),
+      ),
+    );
+  }
+}
